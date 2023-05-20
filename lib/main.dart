6b813f3f50ceecce43c85e261dbdf7c912570316
Loi Tran <datloiboi@gmail.com>
2023-05-20 19:44:21 -0400

    Add basic chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
 
   final List<Widget> _tabs = [
-    const TabScreen(title: 'Tab 1'),
+    StockChart(),
     const TabScreen(title: 'Tab 2'),
     const TabScreen(title: 'Tab 3'),
   ];
@@ -104,7 +107,7 @@ class _MyAppState extends State<MyApp> {
           },
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+              icon: Icon(Icons.auto_graph),
               label: 'Stocks',
             ),
             BottomNavigationBarItem(
@@ -130,7 +133,124 @@ class TabScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(title),
+      child: Text(this.title),
     );
   }
+}
+
+class StockChart extends StatefulWidget {
+  @override
+  _StockChartState createState() => _StockChartState();
+}
+
+class _StockChartState extends State<StockChart> {
+  List<StockData> _stockData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse('<API_URL>'));
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final List<StockData> fetchedData = [];
+        for (var data in jsonData) {
+          fetchedData.add(
+            StockData(
+              DateTime.parse(data['date']),
+              double.parse(data['low']),
+              double.parse(data['high']),
+              double.parse(data['open']),
+              double.parse(data['close']),
+            ),
+          );
+        }
+        setState(() {
+          _stockData = fetchedData;
+        });
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (error) {
+      print('Error occurred: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      padding: EdgeInsets.all(16),
+      child: SfCartesianChart(
+        primaryXAxis: DateTimeAxis(),
+        series: <ChartSeries>[
+          CandleSeries<StockData, DateTime>(
+            dataSource: <StockData>[
+              StockData(
+                DateTime(2022, 1, 1),
+                150.0,
+                200.0,
+                125.0,
+                175.0,
+              ),
+              StockData(
+                DateTime(2022, 1, 2),
+                175.0,
+                250.0,
+                150.0,
+                225.0,
+              ),
+              StockData(
+                DateTime(2022, 1, 3),
+                225.0,
+                275.0,
+                200.0,
+                250.0,
+              ),
+              StockData(
+                DateTime(2022, 1, 4),
+                250.0,
+                300.0,
+                225.0,
+                275.0,
+              ),
+              StockData(
+                DateTime(2022, 1, 5),
+                275.0,
+                350.0,
+                250.0,
+                325.0,
+              ),
+              StockData(
+                DateTime(2022, 1, 6),
+                325.0,
+                400.0,
+                300.0,
+                375.0,
+              ),
+            ],
+            xValueMapper: (StockData stock, _) => stock.date,
+            lowValueMapper: (StockData stock, _) => stock.low,
+            highValueMapper: (StockData stock, _) => stock.high,
+            openValueMapper: (StockData stock, _) => stock.open,
+            closeValueMapper: (StockData stock, _) => stock.close,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StockData {
+  final DateTime date;
+  final double low;
+  final double high;
+  final double open;
+  final double close;
+
+  StockData(this.date, this.low, this.high, this.open, this.close);
 }
