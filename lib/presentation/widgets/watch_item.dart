a0cbf001c491eaf9821494fa74d8c41d89c2965d
Loi Tran <datloiboi@gmail.com>
2023-06-27 19:53:09 -0400

    Make watch_item beautiful on mobile.

--- lib/presentation/widgets/watch_item.dart
+++ lib/presentation/widgets/watch_item.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,17 +14,160 @@ class WatchItem extends StatefulWidget {
 }
 
 class WatchItemState extends State<WatchItem> {
+  Widget buildSmallChart(BuildContext context, color, data, large) {
+    return IgnorePointer(
+      child: SizedBox(
+        height: large ? 100 : 70,
+        width: large ? 250 : 175,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 0,
+              color: Colors.transparent,
+            ),
+          ),
+          child: SfCartesianChart(
+            plotAreaBorderWidth: 0,
+            margin: EdgeInsets.zero,
+            borderColor: Colors.transparent,
+            primaryXAxis: NumericAxis(
+              isVisible: false,
+            ),
+            primaryYAxis: NumericAxis(
+              isVisible: false,
+              majorGridLines: const MajorGridLines(
+                width: 2,
+                dashArray: <double>[4, 3],
+              ),
+              plotBands: [
+                PlotBand(
+                  opacity: 0.5,
+                  borderWidth: 1,
+                  end: data.last.y,
+                  start: data.last.y,
+                  dashArray: const [4, 3],
+                  borderColor: T(context, 'inversePrimary'),
+                ),
+              ],
+            ),
+            series: <ChartSeries>[
+              LineSeries<ChartData, int>(
+                  dataSource: data,
+                  xValueMapper: (ChartData data, _) => data.x,
+                  yValueMapper: (ChartData data, _) => data.y,
+                  color: color
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget buildSmall(context, navigate, data, item, color) {
+    // The nested container with transparent color
+    // ensures that a tap on the entire row results in a navigate.
+    // Without a color the tap on the container is not registered and navigation fails.
+    return GestureDetector(
+      onTap: () {
+        navigate();
+      },
+      child: SizedBox(
+        height: 100,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Container(
+            color: Colors.transparent,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    // color: Colors.green.shade50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5),
+                          child: Text(
+                            item.sym,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          item.shares.toString(),
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    // color: Colors.lightBlue.shade50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        buildSmallChart(context, color, data, false),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    // color: Colors.red.shade50,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () {
+                            navigate();
+                          },
+                          style: ButtonStyle(
+                            minimumSize: MaterialStateProperty.all(
+                              const Size(100, 35),
+                            ),
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                              color,
+                            ),
+                            side: MaterialStateProperty.all<BorderSide>(
+                              BorderSide(
+                                color: color,
+                              ),
+                            ),
+                          ),
+                          child: Text(formatMoney(item.price.toString())),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Context when passed to buildSmall is not the same as the context
-    // So we define the navigate here and pass it to the button.
+    // Context when passed to buildSmall is not the same as the
+    // context so we define navigate here and pass it to the button.
     navigate() {
       context.go("/securities/${widget.item.sym}");
     }
 
-    if (isS(context)) {
-      return buildSmall(context, navigate);
-    }
     final List<ChartData> data = [
       ChartData(2010, randomInt(20, 40)),
       ChartData(2011, randomInt(20, 40)),
@@ -33,9 +175,12 @@ class WatchItemState extends State<WatchItem> {
       ChartData(2013, randomInt(20, 40)),
       ChartData(2014, randomInt(20, 40))
     ];
-
     final item = widget.item;
     final color = data.first.y < data.last.y ? Colors.green : Colors.red;
+
+    if (isS(context)) {
+      return buildSmall(context, navigate, data, item, color);
+    }
     return HoverDarken(
       radius: false,
       padding: false,
@@ -111,139 +256,6 @@ class WatchItemState extends State<WatchItem> {
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildSmallChart(BuildContext context, color, data, large) {
-    return IgnorePointer(
-      child: SizedBox(
-        height: large ? 100 : 70,
-        width: large ? 250 : 100,
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 0,
-              color: Colors.transparent,
-            ),
-          ),
-          child: SfCartesianChart(
-            plotAreaBorderWidth: 0,
-            borderColor: Colors.transparent,
-            primaryXAxis: NumericAxis(
-              isVisible: false,
-            ),
-            primaryYAxis: NumericAxis(
-              isVisible: false,
-              majorGridLines: const MajorGridLines(
-                width: 2,
-                dashArray: <double>[4, 3],
-              ),
-              plotBands: [
-                PlotBand(
-                  opacity: 0.5,
-                  borderWidth: 1,
-                  end: data.last.y,
-                  start: data.last.y,
-                  dashArray: const [4, 3],
-                  borderColor: T(context, 'inversePrimary'),
-                ),
-              ],
-            ),
-            series: <ChartSeries>[
-              LineSeries<ChartData, int>(
-                dataSource: data,
-                xValueMapper: (ChartData data, _) => data.x,
-                yValueMapper: (ChartData data, _) => data.y,
-                color: color
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildSmall(context, navigate) {
-    final List<ChartData> data = [
-      ChartData(2010, randomInt(20, 40)),
-      ChartData(2011, randomInt(20, 40)),
-      ChartData(2012, randomInt(20, 40)),
-      ChartData(2013, randomInt(20, 40)),
-      ChartData(2014, randomInt(20, 40))
-    ];
-
-    final item = widget.item;
-    final color = data.first.y < data.last.y ? Colors.green : Colors.red;
-    return SizedBox(
-      height: 100,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 5),
-                    child: Text(
-                      item.sym,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    item.shares.toString(),
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  buildSmallChart(context, color, data, false),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OutlinedButton(
-                    onPressed: () {
-                      navigate();
-                    },
-                    style: ButtonStyle(
-                      minimumSize: MaterialStateProperty.all(
-                        const Size(100, 35),
-                      ),
-                      foregroundColor: MaterialStateProperty.all<Color>(
-                        color,
-                      ),
-                      side: MaterialStateProperty.all<BorderSide>(
-                        BorderSide(
-                          color: color,
-                        ),
-                      ),
-                    ),
-                    child: Text(formatMoney(item.price.toString())),
-                  ),
-                ],
-              ),
-            )
-          ],
         ),
       ),
     );
